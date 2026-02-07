bf4a09c480f530012a0e751e28aaf75936109ed2
Eman-Gabr25 <emangabr191@gmail.com>
2026-02-07 23:38:45 +0200

    done

--- task1.js
+++ task1.js
@@ -1,2 +1,17 @@
-document.write("hello world") ;
-console.log("hello world");
+function multiplyBy(factor)
+ {
+    return function (number) 
+    {
+        return number * factor;
+    };
+}
+
+const double = multiplyBy(2);
+const triple = multiplyBy(3);
+
+for (let i = 1; i <= 10; i++)
+     {
+    console.log("Number:", i);
+     console.log("Double =", double(i));
+    console.log("Triple =", triple(i));
+     }
